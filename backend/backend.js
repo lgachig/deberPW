07ce6617fd59ff1d85cfe75689ab741c43cfffde
lgachig <163140225+lgachig@users.noreply.github.com>
2025-10-31 11:50:22 -0500

    add new fuction, we find dog

--- backend/backend.js
+++ backend/backend.js
@@ -18,7 +18,25 @@ app.get('/', async (req, res) => {
     }
 });
 
-const PORT = 3004;
+app.get('/breed', async (req, res) => {
+    try {
+      const nameQuery = req.query.name?.toLowerCase() || '';
+      const response = await axios.get(httpapi);
+  
+      const breeds = response.data.data.filter(breed =>
+        breed.attributes.name.toLowerCase().includes(nameQuery)
+      );
+  
+      res.json({ data: breeds });
+    } catch (error) {
+      console.error(`Error from API: ${error.message}`);
+      res.status(500).json({ error: 'Failed to fetch data from API' });
+    }
+  });
+  
+
+
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
